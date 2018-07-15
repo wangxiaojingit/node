62148990d3c874635c0988979c4764d10e989021
zhufengwangxiaojin <547154109@qq.com>
2018-07-15 23:33:31 +0800

    aa

--- 11stream/原理/写流/writeStream.js
+++ 11stream/原理/写流/writeStream.js
@@ -1,46 +1,84 @@
-let path=require("path");
-let fs=require("fs");
-let EventEmitter=require("events");
+let fs = require('fs');
+let EventEmitter = require('events');
 
 class WriteStream extends EventEmitter{
-    constructor(path,options){
-       this.path=path;
-       this.flags=options.flags||"w";
-       this.mode=options.mode||0o666;
-       this.autoClose=options.autoClose||true;
-       this.encoding=options.encoding||"utf8";
-       this.start=options.start||0;
-       this.highWaterMark=options.highWaterMark;
-       //值为false的时候,真正写入内存中,如果这个值是true的时候,就要写到缓存中
-       this.writing=false;
-       //存放到缓存中的数组;
-       this.buffer=[];
-       //存放缓存中的buffer的长度累积
-       this.len=0;
-       //存一个开始的位置
-       this.pos=this.start;
-       //false 的时候不触发抽干事件,true的时候才开始触发抽干事件.
-       this.needDrain=false;
-       this.open();
+  constructor(path,options ={}){
+    super();
+    this.path = path;
+    this.flags = options.flags || 'w';
+    this.mode = options.mode || 0o666;
+    this.highWaterMark = options.highWaterMark || 16*1024;
+    this.start = options.start || 0;
+    this.autoClose = options.autoClose|| true;
+    this.encoding = options.encoding || 'utf8';
+
+    // 是否需要触发drain事件
+    this.needDrain = false;
+    // 是否正在写入
+    this.writing = false;
+    // 缓存 正在写入就放到缓存中
+    this.buffer = [];
+    // 算一个当前缓存的个数
+    this.len = 0;
+    // 写入的时候也有位置关系
+    this.pos = this.start;
+    this.open();
+  }
+  // 0 [1 2] 
+  write(chunk, encoding = this.encoding,callback){
+    chunk = Buffer.isBuffer(chunk)?chunk:Buffer.from(chunk);
+    this.len += chunk.length;// 每次调用write就统计一下长度
+    this.needDrain = this.highWaterMark <= this.len; 
+    // this.fd
+    if(this.writing){
+      this.buffer.push({chunk,encoding,callback});
+    }else{
+      // 当文件写入后 清空缓存区的内容
+      this.writing = true;  // 走缓存
+      this._write(chunk,encoding,()=>this.clearBuffer());
     }
-     destory(){
-       if(typeof this.fd=="number"){
-          fs.close(this.fd,()=>{
-              this.emit("close");
-          });
-          return;
-       }
-       this.emit("close");
-     }
-     open(){
-         fs.open(this.path,this.flags,this.mode,(err,fd)=>{
-             if(err){
-                fs.emit("error");
-                this.destory();//关掉文件
-                return
-             }
-             this.fd=fd;
-             fs.emit("open");//触发打开文件
-         })
-     }
+    return !this.needDrain; // write 的返回值必须是true / false
+  }
+  _write(chunk,encoding,callback){
+    if (typeof this.fd !== 'number') {
+      return this.once('open', () => this._write(chunk, encoding, callback));
+    }
+    // fd是文件描述符 chunk是数据 0 写入的位置和 长度 , this.pos偏移量
+    fs.write(this.fd, chunk,0,chunk.length,this.pos,(err,bytesWritten)=>{
+      this.pos += bytesWritten;
+      this.len -= bytesWritten; // 写入的长度会减少
+      callback();
+    });
+  }
+  clearBuffer(){
+    let buf = this.buffer.shift();
+    if(buf){
+      this._write(buf.chunk, buf.encoding, () => this.clearBuffer());
+    }else{
+      this.writing = false;
+      this.needDrain = false; // 触发一次drain  再置回false 方便下次继续判断
+      this.emit('drain');
+    }
+  }
+  destroy(){
+    if(typeof this.fd === 'number'){
+      fs.close(this.fd,()=>{
+        this.emit('close');
+      });
+      return 
+    }
+    this.emit('close');
+  }
+  open(){
+    fs.open(this.path,this.flags,this.mode,(err,fd)=>{
+      if(err){
+        this.emit('error');
+        this.destroy();
+        return 
+      }
+      this.fd = fd;
+      this.emit('open');
+    });
+  }
 }
+module.exports=WriteStream;
